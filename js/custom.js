0dc73d0e50eb5ba1cf3e48d9c1a717c313669c8a
RifatSanaullah <refatsanaullah@gmail.com>
2019-11-06 13:48:14 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+$( document ).ready( function() {	
+	
+	/*================== start color changer====================*/
+	$(".red").click(function(){
+		$("body").css('background-color','red');
+	});
+	$(".purple").click(function(){
+		$("body").css('background-color','purple');
+	});
+	$(".white").click(function(){
+		$("body").css('background-color','white');
+	});
+	$(".yellow").click(function(){
+		$("body").css('background-color','yellow');
+	});
+	$(".blue").click(function(){
+		$("body").css('background-color','blue');
+	});
+	$(".deeppink").click(function(){
+		$("body").css('background-color','deeppink');
+	});
+	/*================== end color changer====================*/
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
